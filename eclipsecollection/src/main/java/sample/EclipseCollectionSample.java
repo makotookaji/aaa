7ac8de8b1bb78cf748b6ee2f535eccaf95acced9
JP11540 <JP11540@G010391308093.nhnjp.local>
2017-05-30 20:18:54 +0900

    add sample method

--- eclipsecollection/src/main/java/sample/EclipseCollectionSample.java
+++ eclipsecollection/src/main/java/sample/EclipseCollectionSample.java
@@ -9,18 +9,24 @@ import org.eclipse.collections.impl.factory.Lists;
 
 public class EclipseCollectionSample {
 	public static void main(String[] args) {
-		MutableList<Map<String, Object>> mapList = Lists.mutable.empty();
 
+		MutableList<Map<String, Object>> mapList = Lists.mutable.empty();
 		for (int i = 0; i < 1000; i++) {
 			Map<String, Object> map = new HashMap<>();
 			map.put("itemcode", "dfj;lajeflwa" + i);
+			map.put("id", i);
 			mapList.add(map);
 		}
 
 		Iterator<String> str = mapList.collect(map -> (String) map.get("itemcode")).iterator();
 		while (str.hasNext()) {
-			System.out.println(str.next());
+			System.out.println("itemcode" + str.next());
 		}
 
+		mapList = mapList.select(map -> (Integer) map.get("id") % 3 == 0 || (Integer) map.get("id") % 5 == 0);
+		Iterator<Map<String, Object>> iterator = mapList.iterator();
+		while (iterator.hasNext()) {
+			System.out.println("id:" + iterator.next().get("id"));
+		}
 	}
 }
